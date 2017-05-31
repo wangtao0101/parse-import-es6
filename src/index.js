5d257d1ce73b6590648018b42478238314e6112c
wangtao0101 <yuecjn@gmail.com>
2017-05-31 17:37:00 +0800

    clean code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import parseImport from './parseImport';
 
 export default parseImport;
-
-const p = `
-            /*
-            i am a comment, one */ //i am a comment, two
-            import a, { b, c as d } from 'aa';
-        `;
-console.log(parseImport(p));
